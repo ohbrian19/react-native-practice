cd213b7fff3cdf01885f05a270414f79241b0ea4
Brian <ohbrian12@gmail.com>
2020-11-08 17:54:02 -0500

    added image location to ListingScreen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
-import LoginScreen from "./app/screens/LoginScreen";
-import MessageScreen from "./app/screens/MessageScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
+import Screen from "./app/components/Screen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
   return <ListingEditScreen />;
